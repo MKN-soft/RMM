6203ba22f5da525d11b64d33a571c1225dc206f4
pyciak666 <marcin.ryciak@gmail.com>
2015-04-26 17:20:48 +0200

    Revert "Nav Drawer"
    This reverts commit 3c2bec86f4ced9dafe3c67370e913b1629d0a1f7.

--- app/src/main/java/pz2015/habits/rmm/MainActivity.java
+++ app/src/main/java/pz2015/habits/rmm/MainActivity.java
@@ -1,7 +1,6 @@
 package pz2015.habits.rmm;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
