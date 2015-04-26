980eadcc3b9f427f9ec91f52f9be30e3fd73614c
Hippo14 <hippo14@gmail.com>
2015-04-27 00:23:09 +0200

    Build v0.04
    dodana lista do home fragment

--- app/src/main/java/pz2015/habits/rmm/HomeFragment.java
+++ app/src/main/java/pz2015/habits/rmm/HomeFragment.java
@@ -5,12 +5,29 @@ package pz2015.habits.rmm;
  */
 
 import android.app.Fragment;
+import android.app.ListActivity;
+import android.app.ListFragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    private ListView habitListView;
+    private ArrayAdapter habitItemArrayAdapter;
+    //private List<Habit> habits = new ArrayList();
+    private String[] stringArray;
+
+    private View view;
 
     public HomeFragment(){}
 
@@ -20,6 +37,17 @@ public class HomeFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        stringArray = new String[10];
+        for(int i=0; i < stringArray.length; i++){
+            stringArray[i] = "String " + i;
+        }
+
+        habitItemArrayAdapter = new ArrayAdapter(rootView.getContext(), android.R.layout.simple_list_item_1, stringArray);
+        habitListView = (ListView) rootView.findViewById(R.id.habitList);
+        habitListView.setAdapter(habitItemArrayAdapter);
+
+
         return rootView;
     }
+
 }
