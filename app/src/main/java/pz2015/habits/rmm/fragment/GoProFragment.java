cbe278f289dbbfbc9b1724b448285b6665f65ac9
pyciak666 <marcin.ryciak@gmail.com>
2015-05-29 00:27:40 +0200

    GoPro fragment with listeners - this time it works
    just foundation, nothing to see here.

--- app/src/main/java/pz2015/habits/rmm/fragment/GoProFragment.java
+++ app/src/main/java/pz2015/habits/rmm/fragment/GoProFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import pz2015.habits.rmm.R;
 
@@ -17,7 +19,33 @@ public class GoProFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_go_pro, container, false);
 
+        //adding onclick listeners
+        TextView text = (TextView) rootView.findViewById(R.id.txt_goPro);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                billing();
+            }
+        });
+
+        ImageView iView = (ImageView) rootView.findViewById(R.id.image_goPro);
+        iView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                billing();
+            }
+        });
+
         return rootView;
+    }
+
+    /**
+     * manages micropayments (billing), is called when specific button or textview is pressed.
+     * Called by onClick defined in onCreateView
+     */
+    private void billing() {
+        int x = 5; // This is not the code you are looking for. Just for debugging purposes. You can go.
+        //TODO: implement in-app billing
     }
 
 }
