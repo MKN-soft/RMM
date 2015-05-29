8e7b5273983522f3449349e46fa116e1c870e589
pyciak666 <marcin.ryciak@gmail.com>
2015-05-29 20:38:09 +0200

    Build v0.17.3 - billing foundation
    Dodana paczka com.android.vending.billing - w tym plik aidl w nowym
    folderze aidl. Rebuild wygenerował plik java w /generated
    
    Dodane pliki java w tej samej paczce jako podstawa billingu
    
    Dodane pozwolenie na billing w AndroidManifest

--- app/src/main/java/pz2015/habits/rmm/fragment/GoProFragment.java
+++ app/src/main/java/pz2015/habits/rmm/fragment/GoProFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.android.vending.billing.IInAppBillingService;
 
 import pz2015.habits.rmm.R;
 
